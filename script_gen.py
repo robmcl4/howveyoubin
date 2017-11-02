92aadd407c191aedad7a932582347706590164d5
Kevin <kph1958@rit.edu>
2017-11-02 14:31:10 -0400

    Command line arguments, chmod +x, shebang... the works

--- script_gen.py
+++ script_gen.py
@@ -1,48 +1,49 @@
+#!/usr/bin/python3
 """
 Generates runtime script
 """
 
+import argparse
 import csv
 import random
 
-RANDOM_SEED = 1
-
-# number records to generate
-NUM_RECORDS = 10000
-
-# avg inter-arrival time between requests, seconds
-REQUEST_ARRIVAL_RATE = 0.3
-
-# uniform-distribution range for request quantities (upper bound exclusive)
-REQUEST_RANGE = (1, 10)
-
-# avg inter-arrival time between restocks, seconds
-RESTOCK_ARRIVAL_RATE = 100
-
-# uniform-distribution range for request restocks
-RESTOCK_RANGE = (2000, 2100)
-
-# output file name
-FNAME = 'test_script.csv'
+def handle_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num_records", type=int, default=10000,
+            help="The number of records to generate")
+    parser.add_argument("-r", "--request_arrival_rate", type=float, default=0.3,
+            help="Average inter arrival time between requests, in seconds")
+    parser.add_argument("-a", "--request_range", type=tuple, default=(1,10),
+            help="Uniform distribution range for request quantities")
+    parser.add_argument("-s", "--restock_arrival_rate", type=float, default=100,
+            help="Average inter arrival time between restocks, in seconds")
+    parser.add_argument("-b", "--restock_range", type=tuple, default=(2000, 2100),
+            help="Uniform distribution range for request restocks")
+    parser.add_argument("-f","--filename", type=str, default='test_script.csv',
+            help="Name for output csv file")
+    parser.add_argument("-S","--seed", type=int, default=1,
+            help="Seed for PRNG")
+    return parser.parse_args()
 
 def main():
-    random.seed(RANDOM_SEED)
-    with open(FNAME, mode='w', newline='') as f:
+    args = handle_arguments()
+    random.seed(args.seed)
+    with open(args.filename, mode='w', newline='') as f:
         writer = csv.writer(f)
         # start with a restock
-        writer.writerow(['+', 0.0, random.randrange(*RESTOCK_RANGE)])
-        next_restock = random.expovariate(1 / RESTOCK_ARRIVAL_RATE)
-        next_request = random.expovariate(1 / REQUEST_ARRIVAL_RATE)
-        for _ in range(NUM_RECORDS):
+        writer.writerow(['+', 0.0, random.randrange(*args.restock_range)])
+        next_restock = random.expovariate(1 / args.restock_arrival_rate)
+        next_request = random.expovariate(1 / args.request_arrival_rate)
+        for _ in range(args.num_records):
             # figure out if we do a restock or a request next
             if next_restock < next_request:
                 # do restock
-                writer.writerow(['+', next_restock, random.randrange(*RESTOCK_RANGE)])
-                next_restock = next_restock + random.expovariate(1 / RESTOCK_ARRIVAL_RATE)
+                writer.writerow(['+', next_restock, random.randrange(*args.restock_range)])
+                next_restock = next_restock + random.expovariate(1 / args.restock_arrival_rate)
             else:
                 # do request
-                writer.writerow(['-', next_request, random.randrange(*REQUEST_RANGE)])
-                next_request = next_request + random.expovariate(1 / REQUEST_ARRIVAL_RATE)
+                writer.writerow(['-', next_request, random.randrange(*args.request_range)])
+                next_request = next_request + random.expovariate(1 / args.request_arrival_rate)
 
-    
+
 main()
